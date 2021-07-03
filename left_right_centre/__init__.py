a2a38562e847bbed399a48cd52d2ffa7830658a2
rursino <rosscursino@gmail.com>
2021-07-04 09:56:01 +1000

    Build setup tools

--- left_right_centre/__init__.py
+++ left_right_centre/__init__.py
@@ -1,1 +1,2 @@
 from .core import Game, Player
+from .version import __version__
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from left_right_centre import __version__
 
 setup(
     name='left_right_centre',
-    'version': 0.0.1,
+    version=__version__,
     description='A simple game of Left, Right, Centre with ability ot perform simulations.',
     author='Ross Ursino',
     packages=find_packages()   
