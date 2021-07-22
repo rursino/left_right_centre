da942bb819e6ae0b040d8c2de8dbdb4e966b1733
Ross <rosscursino@gmail.com>
2021-07-22 20:20:25 +1000

    Prepare setup.py to add bin files

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+import os
 from setuptools import setup, find_packages
 from left_right_centre import __version__
+
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+BIN_FILES = [
+    'bin/%s' % f for f in os.listdir(os.path.join(SCRIPT_DIR, 'bin'))
+    if f[0] != '.'
+]
+
 
 setup(
     name='left_right_centre',
     version=__version__,
     description='A simple game of Left, Right, Centre with ability to perform simulations.',
     author='Ross Ursino',
-    packages=find_packages()   
+    packages=find_packages(),
+    scripts=BIN_FILES
 )
